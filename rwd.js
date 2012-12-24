e09281cf3c517b87992b2b952036cde0629d1477
phil <lemonadedrink@gmail.com>
2012-12-24 15:45:16 +0800

    transition version of nav, only for webkit

--- rwd.js
+++ rwd.js
@@ -2,19 +2,32 @@ $(function () {
   var isNav = $("#isNav"),
   nav = $("#nav"),
   slide = $("#slide"),
-  slideItem = slide.find("li");
+  slideItem = slide.find("li"),
+  size = window.getComputedStyle(document.body, ':after').getPropertyValue('content');
+  nav.height(0);
   isNav.on("click", function () { //add click hanlder for navigation
     var navHeight = nav.height();
     if (isNav.is(".open")) {
       isNav.removeClass("open");
+      nav.height(0);
+      /*
       nav.stop(true, false)
       .animate({
         height : 0
       }, "fast", function () {
         nav.attr("style", "display: none");
-      });
+      });*/
     } else {
       isNav.addClass("open");
+      nav.height("auto");
+      if(nav.height() !== 0) {
+        nav.height(nav.height());
+      }
+      /*
+      if(nav[0].clientHeight !== 0) {
+        nav.height(nav[0].clientHeight);
+      }*/
+      /*
       nav.show()
       .height(0)
       .stop(true, false)
@@ -22,11 +35,17 @@ $(function () {
         height : navHeight
       }, "fast", function () {
         nav.attr("style", "display: block");
-      });
+      });*/
     }
   });
   $(".slide_item").on("click", function (e) { // when each slide is clicked
     e.preventDefault();
   });
   slideItem.eq(0).addClass("currentSlide");
+
+  if(size.indexOf('middlescreen') !== -1) {
+    //console.info(size, size == "middlescreen");
+    // Load some more content.
+  }
+  
 });
